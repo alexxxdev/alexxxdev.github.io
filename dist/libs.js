cca69b31260547e419bd49ae968818ca7130a650
alexxxdev <alexxxdev@yandex.ru>
2016-09-23 10:41:21 +0700

    added libs in libs.js

--- dist/libs.js
+++ dist/libs.js
@@ -304,7 +304,7 @@ var libs = [
         url: "http://square.github.io/dagger/",
         github: "https://github.com/square/dagger",
         desc: "A fast dependency injector for Android and Java",
-        gradle: "com.squareup.dagger:dagger:<version>'",
+        gradle: "compile 'com.squareup.dagger:dagger:<version>'",
         license: "Apache License V2",
         license_url: "https://www.apache.org/licenses/LICENSE-2.0",
         bintray_latest_version: "https://bintray.com/bintray/jcenter/com.squareup.dagger:dagger/_latestVersion",
@@ -445,21 +445,6 @@ var libs = [
     ]
   },
   {
-    name: "Other",
-    list: [
-      {
-        name: "joda-time-android",
-        github: "https://github.com/dlew/joda-time-android",
-        desc: "Joda-Time library with Android specialization",
-        gradle: "compile 'net.danlew:android.joda:<version>'",
-        license: "Apache License V2",
-        license_url: "https://www.apache.org/licenses/LICENSE-2.0",
-        bintray_latest_version: "https://bintray.com/bintray/jcenter/net.danlew:android.joda/_latestVersion",
-        bintray_img_version: "https://api.bintray.com/packages/bintray/jcenter/net.danlew:android.joda/images/download.svg"
-      }
-    ]
-  },
-  {
     name: "Google Play Services",
     list: [
       {
@@ -568,7 +553,7 @@ var libs = [
       {
         name: "Multidex Support Library",
         desc: "This library provides support for building apps with multiple Dalvik Executable (DEX) files. Apps that reference more than 65536 methods are required to use multidex configurations",
-        gradle: "com.android.support:multidex:1.0.0'"
+        gradle: "compile 'com.android.support:multidex:1.0.0'"
       },
       {
         name: "v4",
@@ -576,27 +561,27 @@ var libs = [
           {
             name: "compat library",
             desc: "Provides compatibility wrappers for a number of framework APIs",
-            gradle: "com.android.support:support-compat:24.2.0"
+            gradle: "compile 'com.android.support:support-compat:24.2.0'"
           },
           {
             name: "core-utils library",
             desc: "Provides a number of utility classes",
-            gradle: "com.android.support:support-core-utils:24.2.0"
+            gradle: "compile 'com.android.support:support-core-utils:24.2.0'"
           },
           {
             name: "core-ui library",
             desc: "Implements a variety of UI-related components",
-            gradle: "com.android.support:support-core-ui:24.2.0"
+            gradle: "compile 'com.android.support:support-core-ui:24.2.0'"
           },
           {
             name: "media-compat library",
             desc: "Backports portions of the media framework, including MediaBrowser and MediaSession",
-            gradle: "com.android.support:support-media-compat:24.2.0"
+            gradle: "compile 'com.android.support:support-media-compat:24.2.0'"
           },
           {
             name: "fragment library",
             desc: "Adds support for encapsulation of user interface and functionality with fragments, enabling applications to provide layouts that adjust between small and large-screen devices. This module has dependencies on compat, core-utils, core-ui, and media-compat.",
-            gradle: "com.android.support:support-fragment:24.2.0"
+            gradle: "compile 'com.android.support:support-fragment:24.2.0'"
           }
         ]
       },
@@ -606,49 +591,49 @@ var libs = [
           {
             name: "appcompat library",
             desc: "This library adds support for the Action Bar user interface design pattern. This library includes support for material design user interface implementations.",
-            gradle: "com.android.support:appcompat-v7:24.2.0"
+            gradle: "compile 'com.android.support:appcompat-v7:24.2.0'"
           },
           {
             name: "cardview library",
             desc: "This library adds support for the CardView widget, which lets you show information inside cards that have a consistent look on any app. These cards are useful for material design implementations, and are used extensively in layouts for TV apps.",
-            gradle: "com.android.support:cardview-v7:24.2.0"
+            gradle: "compile 'com.android.support:cardview-v7:24.2.0'"
           },
           {
             name: "gridlayout library",
             desc: "After you download the Android Support Libraries, this library adds support for the GridLayout class, which allows you to arrange user interface elements using a grid of rectangular cells. For detailed information about the v7 gridlayout library APIs, see the android.support.v7.widget package in the API reference.",
-            gradle: "com.android.support:gridlayout-v7:24.2.0"
+            gradle: "compile 'com.android.support:gridlayout-v7:24.2.0'"
           },
           {
             name: "mediarouter library",
             desc: "This library provides MediaRouter, MediaRouteProvider, and related media classes that support Google Cast.",
-            gradle: "com.android.support:mediarouter-v7:24.2.0"
+            gradle: "compile 'com.android.support:mediarouter-v7:24.2.0'"
           },
           {
             name: "palette library",
             desc: "The v7 palette support library includes the Palette class, which lets you extract prominent colors from an image.",
-            gradle: "com.android.support:palette-v7:24.2.0"
+            gradle: "compile 'com.android.support:palette-v7:24.2.0'"
           },
           {
             name: "recyclerview library",
             desc: "The recyclerview library adds the RecyclerView class. This class provides support for the RecyclerView widget, a view for efficiently displaying large data sets by providing a limited window of data items.",
-            gradle: "com.android.support:recyclerview-v7:24.2.0"
+            gradle: "compile 'com.android.support:recyclerview-v7:24.2.0'"
           },
           {
             name: "Preference Support Library",
             desc: "The preference package provides APIs to support adding preference objects, such as CheckBoxPreference and ListPreference, for users to modify UI settings.",
-            gradle: "com.android.support:preference-v7:24.2.0"
+            gradle: "compile 'com.android.support:preference-v7:24.2.0'"
           }
         ]
       },
       {
         name: "v13 Support Library",
         desc: "This library is designed to be used for Android 3.2 (API level 13) and higher. It adds support for the Fragment user interface pattern with the (FragmentCompat) class and additional fragment support classes. For more information about fragments, see the Fragments developer guide. For detailed information about the v13 Support Library APIs, see the android.support.v13 package in the API reference.",
-        gradle: "com.android.support:support-v13:24.2.0'"
+        gradle: "compile 'com.android.support:support-v13:24.2.0'"
       },
       {
         name: "v14 Preference Support Library",
         desc: "The android.support.v14.preference package provides APIs to add support for preference interfaces such as PreferenceFragment.OnPreferenceStartFragmentCallback and PreferenceFragment.OnPreferenceStartScreenCallback, along with classes, such as MultiSelectListPreference and PreferenceFragment. For detailed information about the v14 Preference Support Library APIs, see the preference package in the API reference.",
-        gradle: "com.android.support:preference-v14:24.2.0'"
+        gradle: "compile 'com.android.support:preference-v14:24.2.0'"
       },
       {
         name: "v17",
@@ -656,39 +641,39 @@ var libs = [
           {
             name: "Preference Support Library for TV",
             desc: "The android.support.v17.preference package provides APIs for providing preference interfaces on TV devices, including support for the LeanbackListPreferenceDialogFragment.ViewHolder.OnItemClickListener interface and classes, such as BaseLeanbackPreferenceFragment and LeanbackPreferenceFragment. For detailed information about the v17 Preference Support Library APIs, see the preference package in the API reference.",
-            gradle: "com.android.support:preference-leanback-v17:24.2.0"
+            gradle: "compile 'com.android.support:preference-leanback-v17:24.2.0'"
           },
           {
             name: "Leanback Library",
             desc: "The android.support.v17.leanback package provides APIs to support building user interfaces on TV devices. It provides a number of important widgets for TV apps",
-            gradle: "com.android.support:leanback-v17:24.2.0"
+            gradle: "compile 'com.android.support:leanback-v17:24.2.0'"
           }
         ]
       },
       {
         name: "Annotations Support Library",
         desc: "The Annotation package provides APIs to support adding annotation metadata to your apps.",
-        gradle: "com.android.support:support-annotations:24.2.0'"
+        gradle: "compile 'com.android.support:support-annotations:24.2.0'"
       },
       {
         name: "Design Support Library",
         desc: "The Design package provides APIs to support adding material design components and patterns to your apps.",
-        gradle: "com.android.support:design:24.2.0'"
+        gradle: "compile 'com.android.support:design:24.2.0'"
       },
       {
         name: "Custom Tabs Support Library",
         desc: "The Custom Tabs package provides APIs to support adding and managing custom tabs in your apps.",
-        gradle: "com.android.support:customtabs:24.2.0'"
+        gradle: "compile 'com.android.support:customtabs:24.2.0'"
       },
       {
         name: "Percent Support Library",
         desc: "The Percent package provides APIs to support adding and managing percentage based dimensions in your app.",
-        gradle: "com.android.support:percent:24.2.0'"
+        gradle: "compile 'com.android.support:percent:24.2.0'"
       },
       {
         name: "App Recommendation Support Library for TV",
         desc: "The App Recommendation package provides APIs to support adding content recommendations in your app running on TV devices.",
-        gradle: "com.android.support:recommendation:24.2.0'"
+        gradle: "compile 'com.android.support:recommendation:24.2.0'"
       }
     ]
   },
@@ -708,7 +693,7 @@ var libs = [
         url: "http://mikepenz.github.io/MaterialDrawer/",
         github: "https://github.com/mikepenz/MaterialDrawer",
         desc: "The flexible, easy to use, all in one drawer library for your Android project",
-        gradle: "com.mikepenz:materialdrawer:5.6.0@aar",
+        gradle: "compile 'com.mikepenz:materialdrawer:5.6.0@aar'",
         license: "Apache License V2",
         license_url: "https://www.apache.org/licenses/LICENSE-2.0",
         bintray_latest_version: "https://bintray.com/bintray/jcenter/com.mikepenz:materialdrawer/_latestVersion",
@@ -718,7 +703,7 @@ var libs = [
         name: "AndroidSlidingUpPanel",
         github: "https://github.com/umano/AndroidSlidingUpPanel",
         desc: "This library provides a simple way to add a draggable sliding up panel (popularized by Google Music and Google Maps) to your Android application",
-        gradle: "com.sothree.slidinguppanel:library:<version>",
+        gradle: "compile 'com.sothree.slidinguppanel:library:<version>'",
         license: "Apache License V2",
         license_url: "https://www.apache.org/licenses/LICENSE-2.0",
         bintray_latest_version: "https://bintray.com/bintray/jcenter/com.sothree.slidinguppanel:library/_latestVersion",
@@ -728,7 +713,7 @@ var libs = [
         name: "FlexboxLayout",
         github: "https://github.com/google/flexbox-layout",
         desc: "Flexbox for Android",
-        gradle: "com.google.android:flexbox:<version>",
+        gradle: "compile 'com.google.android:flexbox:<version>'",
         license: "Apache License V2",
         license_url: "https://www.apache.org/licenses/LICENSE-2.0",
         bintray_latest_version: "https://bintray.com/google/flexbox-layout/flexbox/_latestVersion",
@@ -738,11 +723,61 @@ var libs = [
         name: "AppIntro",
         github: "https://github.com/PaoloRotolo/AppIntro",
         desc: "Make a cool intro for your Android app.",
-        gradle: "com.github.paolorotolo:appintro:4.0.0",
+        gradle: "compile 'com.github.paolorotolo:appintro::<version>'",
         license: "Apache License V2",
         license_url: "https://www.apache.org/licenses/LICENSE-2.0",
         bintray_latest_version: "https://bintray.com/rotolopao/maven/com.github.paolorotolo:appintro/_latestVersion",
         bintray_img_version: "https://api.bintray.com/packages/rotolopao/maven/com.github.paolorotolo:appintro/images/download.svg"
+      },
+      {
+        name: "Space-Navigation-View",
+        github: "https://github.com/armcha/Space-Navigation-View",
+        desc: "Space Navigation is a library allowing easily integrate fully customizable Google Spaces like navigation to your app",
+        gradle: "compile 'com.github.armcha:SpaceNavigationView:<version>'",
+        license: "Apache License V2",
+        license_url: "https://www.apache.org/licenses/LICENSE-2.0",
+        bintray_latest_version: "https://bintray.com/armcha/maven/SpaceNavigationView/_latestVersion",
+        bintray_img_version: "https://api.bintray.com/packages/armcha/maven/SpaceNavigationView/images/download.svg"
+      },
+      {
+        name: "SpeedView",
+        github: "https://github.com/anastr/SpeedView",
+        desc: "Dynamic Speedometer for Android. amazing, powerful, and multi shape :zap: , you can change the color of everything",
+        gradle: "compile 'com.github.anastr:speedviewlib:<version>'",
+        license: "Apache License V2",
+        license_url: "https://www.apache.org/licenses/LICENSE-2.0",
+        bintray_latest_version: "https://bintray.com/anastr/maven/SpeedView/_latestVersion",
+        bintray_img_version: "https://api.bintray.com/packages/anastr/maven/SpeedView/images/download.svg"
+      },
+      {
+        name: "SlidingTutorial",
+        github: "https://github.com/Cleveroad/SlidingTutorial-Android",
+        desc: "Sliding Tutorial Library for Android",
+        gradle: "compile 'com.cleveroad:slidingtutorial:<version>'",
+        license: "Apache License V2",
+        license_url: "https://www.apache.org/licenses/LICENSE-2.0",
+        bintray_latest_version: "https://bintray.com/bintray/jcenter/com.cleveroad:slidingtutorial/_latestVersion",
+        bintray_img_version: "https://api.bintray.com/packages/bintray/jcenter/com.cleveroad:slidingtutorial/images/download.svg"
+      },
+      {
+        name: "Coverflow",
+        github: "https://github.com/moondroid/CoverFlow",
+        desc: "Android CoverFlow widget",
+        gradle: "compile 'com.github.moondroid.coverflow:library:<version>'",
+        license: "Apache License V2",
+        license_url: "https://www.apache.org/licenses/LICENSE-2.0",
+        bintray_latest_version: "https://bintray.com/bintray/jcenter/com.github.moondroid.coverflow:library/_latestVersion",
+        bintray_img_version: "https://api.bintray.com/packages/bintray/jcenter/com.github.moondroid.coverflow:library/images/download.svg"
+      },
+      {
+        name: "folding-cell-android",
+        github: "https://github.com/Ramotion/folding-cell-android",
+        desc: "FoldingCell is an expanding content cell inspired by folding paper material https://business.ramotion.com/gthb-casestudies",
+        gradle: "compile 'com.ramotion.foldingcell:folding-cell:<version>'",
+        license: "MIT",
+        license_url: "https://github.com/Ramotion/folding-cell-android/blob/master/LICENSE.md",
+        bintray_latest_version: "https://bintray.com/bintray/jcenter/com.ramotion.foldingcell:folding-cell/_latestVersion",
+        bintray_img_version: "https://api.bintray.com/packages/bintray/jcenter/com.ramotion.foldingcell:folding-cell/images/download.svg"
       }
     ]
   },
@@ -753,11 +788,36 @@ var libs = [
         name: "material-dialogs",
         github: "https://github.com/afollestad/material-dialogs",
         desc: "A beautiful, fluid, and customizable dialogs API",
-        gradle: "com.afollestad.material-dialogs:core:<version>\ncompile 'com.afollestad.material-dialogs:commons:<version>",
+        gradle: "compile 'com.afollestad.material-dialogs:core:<version>'\ncompile 'com.afollestad.material-dialogs:commons:<version>'",
         license: "Apache License V2",
         license_url: "https://www.apache.org/licenses/LICENSE-2.0",
         bintray_latest_version: "https://bintray.com/drummer-aidan/maven/material-dialogs:core/_latestVersion",
         bintray_img_version: "https://api.bintray.com/packages/drummer-aidan/maven/material-dialogs:core/images/download.svg"
+      }
+    ]
+  },
+  {
+    name: "Other",
+    list: [
+      {
+        name: "joda-time-android",
+        github: "https://github.com/dlew/joda-time-android",
+        desc: "Joda-Time library with Android specialization",
+        gradle: "compile 'net.danlew:android.joda:<version>'",
+        license: "Apache License V2",
+        license_url: "https://www.apache.org/licenses/LICENSE-2.0",
+        bintray_latest_version: "https://bintray.com/bintray/jcenter/net.danlew:android.joda/_latestVersion",
+        bintray_img_version: "https://api.bintray.com/packages/bintray/jcenter/net.danlew:android.joda/images/download.svg"
+      },
+      {
+        name: "Calligraphy",
+        github: "https://github.com/chrisjenx/Calligraphy",
+        desc: "Custom fonts in Android an OK way.",
+        gradle: "compile 'uk.co.chrisjenx:calligraphy:<version>'",
+        license: "Apache License V2",
+        license_url: "https://www.apache.org/licenses/LICENSE-2.0",
+        bintray_latest_version: "https://bintray.com/chrisjenx/maven/uk.co.chrisjenx:calligraphy/_latestVersion",
+        bintray_img_version: "https://api.bintray.com/packages/chrisjenx/maven/uk.co.chrisjenx:calligraphy/images/download.svg"
       }
     ]
   },
@@ -770,7 +830,7 @@ var libs = [
         desc: "A memory leak detection library for Android and Java.",
         license: "Apache License V2",
         license_url: "https://www.apache.org/licenses/LICENSE-2.0",
-        gradle: "com.squareup.leakcanary:leakcanary-android:1.4"
+        gradle: "compile 'com.squareup.leakcanary:leakcanary-android:1.4'"
       }
     ]
   },
@@ -806,6 +866,18 @@ var libs = [
         desc: "Telegram for Android source",
         license: "Apache License V2",
         license_url: "https://www.apache.org/licenses/LICENSE-2.0"
+      },
+      {
+        name: "Quill",
+        github: "https://github.com/vickychijwani/quill",
+        desc: "Quill is the beautiful Android app for your Ghost blog",
+        license: "MIT",
+        license_url: "https://github.com/vickychijwani/quill/blob/master/LICENSE"
+      },
+      {
+        name: "Notelin",
+        github: "https://github.com/ImangazalievM/Notelin",
+        desc: "Note-taking app for Android written in Kotlin"
       }
     ]
   }
